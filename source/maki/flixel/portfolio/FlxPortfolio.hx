a0c669e38925a77ec00c39258111fd455f1a507b
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-07-20 13:52:33 -0500

    portfol registry

--- source/maki/flixel/portfolio/FlxPortfolio.hx
+++ source/maki/flixel/portfolio/FlxPortfolio.hx
@@ -1,11 +1,19 @@
 package maki.flixel.portfolio;
 
 import flixel.FlxGame;
+import openfl.events.Event;
 
 class FlxPortfolio extends FlxGame
 {
 	public function new()
 	{
 		super(1280, 720, PortfolioSelect, 60, 60, true, false);
+	}
+
+	override function create(_:Event)
+	{
+		PortfolioRegistry.reload();
+
+		super.create(_);
 	}
 }
---
+++ source/maki/flixel/portfolio/PortfolioRegistry.hx
@@ -0,0 +1,27 @@
+package maki.flixel.portfolio;
+
+import haxe.io.Path;
+import lime.utils.Assets;
+import sys.FileSystem;
+
+class PortfolioRegistry
+{
+	public static var PORTFOLIOS(default, null):Array<String> = [];
+
+	public static final PORTFOLIO_DIRECTORY = 'assets/portfolios';
+
+	public static function reload()
+	{
+		PORTFOLIOS = [];
+
+		for (folder in FileSystem.readDirectory(PORTFOLIO_DIRECTORY))
+		{
+			var metaPath = '$PORTFOLIO_DIRECTORY/portfolio.json';
+
+			if (FileSystem.exists(metaPath))
+				PORTFOLIOS.push(folder);
+		};
+
+		trace('PORTFOLIOS: $PORTFOLIOS');
+	}
+}
