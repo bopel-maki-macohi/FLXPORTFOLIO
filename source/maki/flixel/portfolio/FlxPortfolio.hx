a4b6ddaacb911978c8e9c6c18c70ccf648c4d2b8
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-07-20 13:42:06 -0500

    Initial Commit

---
+++ source/maki/flixel/portfolio/FlxPortfolio.hx
@@ -0,0 +1,11 @@
+package maki.flixel.portfolio;
+
+import flixel.FlxGame;
+
+class FlxPortfolio extends FlxGame
+{
+	public function new()
+	{
+		super(1280, 720, PlayState, 60, 60, true, false);
+	}
+}
