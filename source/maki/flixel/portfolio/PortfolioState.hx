9906e06b12f7f44b2a1028be3aeb7f05c9391260
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-07-21 11:46:07 -0500

    go back

--- source/maki/flixel/portfolio/PortfolioState.hx
+++ source/maki/flixel/portfolio/PortfolioState.hx
@@ -1,5 +1,6 @@
 package maki.flixel.portfolio;
 
+import flixel.FlxG;
 import flixel.FlxState;
 
 class PortfolioState extends FlxState
@@ -7,5 +8,17 @@ class PortfolioState extends FlxState
 	override public function new(portfolioID:String)
 	{
 		super();
+	}
+
+	override function create()
+	{
+		super.create();
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (FlxG.keys.justPressed.ESCAPE) FlxG.switchState(() -> new PortfolioSelect());
+
+		super.update(elapsed);
 	}
 }
